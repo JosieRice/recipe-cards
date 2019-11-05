f303db511ba17851e1e85ad4e726612b61850b90
Craig Rice <craigricetong@gmail.com>
2019-11-05 06:07:04 -0800

    migrated away from apollo boost

--- src/graphql/ApolloClient.tsx
+++ src/graphql/ApolloClient.tsx
@@ -1,4 +1,8 @@
-import ApolloClient from "apollo-boost";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink } from "apollo-link";
 
 let uri = "https://us-central1-staging-or.cloudfunctions.net/api/api";
 
@@ -8,7 +12,22 @@ if (window.location.href === "https://original-recipe.com/") {
 }
 
 const client = new ApolloClient({
-  uri
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri,
+      credentials: "same-origin"
+    })
+  ]),
+  cache: new InMemoryCache()
 });
 
 export default client;
